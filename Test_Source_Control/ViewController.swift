afc20160351d6f987e4289bf9338a4d82969d7b5
James Ouhyoung <James@Jamess-MacBook-Air.local>
2015-05-05 19:21:13 -0700

    Initial Commit

---
+++ Test_Source_Control/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test_Source_Control
+//
+//  Created by James Ouhyoung on 5/5/15.
+//  Copyright (c) 2015 James Ouhyoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
